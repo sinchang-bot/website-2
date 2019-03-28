902a8409aaaf91d3301457f91a6a6dd048acceaa
Dave Calnan <d@ve.ie>
2019-03-28 21:22:12 +0000

    Remove trailing slash from posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -56,11 +56,11 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const postPath = createFilePath({ node, getNode, trailingSlash: false })
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: postPath
     })
   }
 }
